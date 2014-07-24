d75bc684edbd4b8447f7f088a7ad468fd4e5d39c
Curtis Blackwell <iam@curtisblackwell.com>
2014-07-24 20:05:03 +0900

    don't strtolower values

--- pi.var.php
+++ pi.var.php
@@ -143,7 +143,7 @@ class Plugin_var extends Plugin
         if (!empty($this->content)) {
             $value = trim(Parse::template($this->content, $this->context));
         } else {
-            $value = $this->fetchParam(['is', 'value', 'val'], false);
+            $value = $this->fetchParam(['is', 'value', 'val'], false, null, false, false);
         }
 
         return $value;
